f36c6993100e58c691c901f363ddf62be63e0557
Jeferson Rios <jefry1795@hotmail.com>
2026-02-17 15:03:31 -0500

    Refactor equipment routes and add new endpoints

--- app/router/equipments.py
+++ app/router/equipments.py
@@ -5,7 +5,7 @@ from app.core.database import get_db
 from app.crud.permisos import verify_permissions
 from app.router.dependencies import get_current_user
 from app.schemas.users import UserOut
-from app.schemas.equipments import EquipoCreate, EquipoUpdate, EquipoOut
+from app.schemas.equipments import EquipoCreate, EquipoUpdate, EquipoOut, TipoEquipo
 from app.crud import equipments as crud_equipments
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -61,7 +61,24 @@ def get_by_serial_equip(serial: str,
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.get("/all",  response_model=List[EquipoOut])
+@router.get("/by-tipo_equip",  response_model=EquipoOut)
+def get_by_tipo_equip(tipo_equip: TipoEquipo, 
+                        db: Session = Depends(get_db),
+                        user_token: UserOut = Depends(get_current_user)):
+    try:
+        id_rol=user_token.rol_id
+
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        equipo = crud_equipments.get_equipment_by_tipo(db, tipo_equip)
+        if not equipo:
+            raise HTTPException(status_code=404, detail="Equipo no encontrado")
+        return equipo
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/all-equips",  response_model=List[EquipoOut])
 def scan_equipment(db: Session = Depends(get_db),
                    user_token: UserOut = Depends(get_current_user)):
     try:
@@ -77,7 +94,44 @@ def scan_equipment(db: Session = Depends(get_db),
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.put("/{codigo_barras_equip}")
+@router.put("/by_id/{id_equip}")
+def update_equip_by_id(id_equip: int, 
+                 equip: EquipoUpdate, 
+                 db: Session = Depends(get_db),
+                 user_token: UserOut = Depends(get_current_user)):
+    try:
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'actualizar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        success = crud_equipments.update_equip_by_id(db, id_equip, equip)
+        if not success:
+            raise HTTPException(status_code=400, detail="No se pudo actualizar el equipo")
+        return {"message": "Equipo actualizado correctamente"}
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.put("/estado/{id_equip}", status_code=status.HTTP_200_OK)
+def estado_equip(
+    id_eq: str,
+    estado_equip: bool,
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+):
+    try:
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'actualizar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        success = crud_equipments.update_estado_equip(db, id_eq, estado_equip)
+        if not success:
+            raise HTTPException(status_code=404, detail="Usuario no encontrado")
+
+        return {"message": f"Estado del equipo actualizado a {estado_equip}"}
+    except SQLAlchemyError as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+@router.put("/by-cod_barras/{codigo_barras_equip}")
 def update_equip(codigo_barras_equip: str, 
                  equip: EquipoUpdate, 
                  db: Session = Depends(get_db),
@@ -94,36 +148,20 @@ def update_equip(codigo_barras_equip: str,
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-@router.put("/estado/{serial_equipo}", status_code=status.HTTP_200_OK)
-def estado_equip(
-    cod_barras_eq: str,
-    estado_equip: bool,
-    db: Session = Depends(get_db),
-    user_token: UserOut = Depends(get_current_user)
-):
-    try:
-        id_rol = user_token.rol_id
-        if not verify_permissions(db, id_rol, modulo, 'actualizar'):
-            raise HTTPException(status_code=401, detail="Usuario no autorizado")
-        
-        success = crud_equipments.update_estado_equip(db, cod_barras_eq, estado_equip)
-        if not success:
-            raise HTTPException(status_code=404, detail="Usuario no encontrado")
-
-        return {"message": f"Estado del incidente  actualizado a {estado_equip}"}
-    except SQLAlchemyError as e:
-            raise HTTPException(status_code=500, detail=str(e))
-
-
-
-# @router.put("/by_id/{serial_equip}")
-# def update_equip_id(serial_equip: str, equip: EquipoUpdateSinSerial, db: Session = Depends(get_db)):
+# @router.put("/by_serial/{serial_equip}")
+# def update_equip_by_serial(serial_equip: str, 
+#                  equip: EquipoUpdate, 
+#                  db: Session = Depends(get_db),
+#                  user_token: UserOut = Depends(get_current_user)):
 #     try:
-#         success = crud_equipments.update_equip_by_serial(db, serial_equip, equip)
+#         id_rol = user_token.rol_id
+#         if not verify_permissions(db, id_rol, modulo, 'actualizar'):
+#             raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+#         success = crud_equipments.update_equip_by_id(db, serial_equip, equip)
 #         if not success:
-#             raise HTTPException(status_code=400, detail="No se pudo actualizar el equipo por serial")
+#             raise HTTPException(status_code=400, detail="No se pudo actualizar el equipo")
 #         return {"message": "Equipo actualizado correctamente"}
 #     except SQLAlchemyError as e:
 #         raise HTTPException(status_code=500, detail=str(e))
-
 
