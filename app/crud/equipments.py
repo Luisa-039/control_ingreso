fdb1698fcd38d29e5849f68b9008b59ec7f21a13
Jeferson Rios <jefry1795@hotmail.com>
2026-02-17 15:02:48 -0500

    Update equipments.py

--- app/crud/equipments.py
+++ app/crud/equipments.py
@@ -4,13 +4,13 @@ from typing import Optional
 from sqlalchemy.exc import SQLAlchemyError
 import logging
 
-from app.schemas.equipments import EquipoCreate, EquipoUpdate
+from app.schemas.equipments import EquipoCreate, EquipoUpdate, TipoEquipo
 
 logger = logging.getLogger(__name__)
 
 def create_equipment(db: Session, 
                      user: EquipoCreate,
-                     user_token: UserOut = Depends(get_current_user)) -> Optional[bool]:
+                     ) -> Optional[bool]:
     try:
         query = text("""
             INSERT INTO equipos_externos (
@@ -87,43 +87,19 @@ def update_equip_by_cod_barras(db: Session, cod_barras: str, equipment: EquipoUp
         logger.error(f"Error al actualizar el equipo {cod_barras}: {e}")
         raise Exception("Error de base de datos al actualizar el equipo")
     
-def update_estado_equip(db:Session, cod_barras_eq: str, estado_equipo: bool):
+def update_estado_equip(db:Session, id_equip: str, estado_equipo: bool):
     try:
         sentencia = text("""
             UPDATE equipos_externos
             SET estado = :estado_eq
-            WHERE codigo_barras_inv = :cod_barras
+            WHERE id_equipo = :equipo_id
         """)
-        result = db.execute(sentencia, {"estado_eq": estado_equipo, "cod_barras": cod_barras_eq})
+        result = db.execute(sentencia, {"estado_eq": estado_equipo, "equipo_id": id_equip})
         db.commit()
 
         return result.rowcount > 0
 
     except SQLAlchemyError as e:
         db.rollback()
-        logger.error(f"Error al cambiar el estado del equipo {cod_barras_eq}: {e}")
+        logger.error(f"Error al cambiar el estado del equipo {id_equip}: {e}")
         raise Exception("Error de base de datos al cambiar el estado del equipo")
-    
-def update_equip_by_serial(db: Session, serial_equip: str, equipment: EquipoUpdate) -> Optional[bool]:
-    try:
-        equipment_data = equipment.model_dump(exclude_unset=True)
-        if not equipment_data:
-            return False 
-
-        set_clauses = ", ".join([f"{key} = :{key}" for key in equipment_data.keys()])
-        sentencia = text(f"""
-            UPDATE equipos 
-            SET {set_clauses}
-            WHERE serial = :serial_eq
-        """)
-
-        equipment_data["serial_eq"] = serial_equip
-
-        result = db.execute(sentencia, equipment_data)
-        db.commit()
-
-        return result.rowcount > 0
-    except SQLAlchemyError as e:
-        db.rollback()
-        logger.error(f"Error al actualizar el estado del equipo {serial_equip}: {e}")
-        raise Exception("Error de base de datos al actualizar el estado del equipo")
