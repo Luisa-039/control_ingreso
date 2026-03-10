18fb94b7d71c1fe74d55fa273c85223a84af5cf0
Jeferson Rios <jefry1795@hotmail.com>
2026-03-10 10:56:30 -0500

    Implement pagination for equipment retrieval
    Added pagination support for equipment retrieval.

--- app/router/equipments.py
+++ app/router/equipments.py
@@ -1,11 +1,13 @@
+import os
+import shutil
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query, File, UploadFile, Form
 from sqlalchemy.orm import Session
 from app.core.database import get_db
 from app.crud.permisos import verify_permissions
 from app.router.dependencies import get_current_user
 from app.schemas.users import UserOut
-from app.schemas.equipments import EquipoCreate, EquipoUpdate, EquipoOut, TipoEquipo
+from app.schemas.equipments import EquipoCreate, EquipoUpdate, EquipoOut, TipoEquipo, PaginatedEquipos
 from app.crud import equipments as crud_equipments
 from sqlalchemy.exc import SQLAlchemyError
 
@@ -14,7 +16,16 @@ modulo = 7
 
 @router.post("/crear", status_code=status.HTTP_201_CREATED)
 def create_equipo(
-    Equipo: EquipoCreate, 
+    persona_id: int = Form(...),
+    tipo_equipo: str = Form(...),
+    serial: str = Form(...),
+    marca_modelo: str = Form(...),
+    descripcion: str = Form(...),
+    codigo_barras_inv: str = Form(...),
+    fecha_registro: str = Form(...),
+    estado: bool = Form(...),
+    foto_path: UploadFile = File(None),
+     
     db: Session = Depends(get_db),
     user_token: UserOut = Depends(get_current_user)
 ):
@@ -23,7 +34,27 @@ def create_equipo(
         if not verify_permissions(db, id_rol, modulo, 'insertar'):
             raise HTTPException(status_code=401, detail= 'Usuario no autorizado')
         
-        crud_equipments.create_equipment(db, Equipo)
+        file_path = None
+
+        if foto_path:
+            os.makedirs("uploads", exist_ok=True)
+            file_path = f"uploads/{codigo_barras_inv}.jpg"
+
+            with open(file_path, "wb") as buffer:
+                shutil.copyfileobj(foto_path.file, buffer)
+
+        equipo_data = EquipoCreate(
+            persona_id=persona_id,
+            tipo_equipo=tipo_equipo,
+            serial=serial,
+            marca_modelo=marca_modelo,
+            descripcion=descripcion,
+            codigo_barras_inv=codigo_barras_inv,
+            fecha_registro=fecha_registro,
+            estado=estado,
+            foto_path=file_path
+        )
+        crud_equipments.create_equipment(db, equipo_data)
         return {"message": "Equipo registrado correctamente"}
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
@@ -113,7 +144,7 @@ def update_equip_by_id(id_equip: int,
 
 @router.put("/estado/{id_equip}", status_code=status.HTTP_200_OK)
 def estado_equip(
-    id_eq: str,
+    id_equip: str,
     estado_equip: bool,
     db: Session = Depends(get_db),
     user_token: UserOut = Depends(get_current_user)
@@ -123,7 +154,7 @@ def estado_equip(
         if not verify_permissions(db, id_rol, modulo, 'actualizar'):
             raise HTTPException(status_code=401, detail="Usuario no autorizado")
         
-        success = crud_equipments.update_estado_equip(db, id_eq, estado_equip)
+        success = crud_equipments.update_estado_equip(db, id_equip, estado_equip)
         if not success:
             raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
@@ -148,20 +179,31 @@ def update_equip(codigo_barras_equip: str,
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# @router.put("/by_serial/{serial_equip}")
-# def update_equip_by_serial(serial_equip: str, 
-#                  equip: EquipoUpdate, 
-#                  db: Session = Depends(get_db),
-#                  user_token: UserOut = Depends(get_current_user)):
-#     try:
-#         id_rol = user_token.rol_id
-#         if not verify_permissions(db, id_rol, modulo, 'actualizar'):
-#             raise HTTPException(status_code=401, detail="Usuario no autorizado")
+@router.get("/all_equips-pag", response_model=PaginatedEquipos)
+def get_equipements_pag(
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+): 
+    try:        
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
         
-#         success = crud_equipments.update_equip_by_id(db, serial_equip, equip)
-#         if not success:
-#             raise HTTPException(status_code=400, detail="No se pudo actualizar el equipo")
-#         return {"message": "Equipo actualizado correctamente"}
-#     except SQLAlchemyError as e:
-#         raise HTTPException(status_code=500, detail=str(e))
+        skip = (page - 1) * page_size
+        data = crud_equipments.get_all_equipements_pag(db, skip=skip, limit=page_size)
+
+        total = data["total"] #cambiar a total 
+        equipos = data["equipos"] #cambiar a equipements
+
+        return PaginatedEquipos(
+            page= page,
+            page_size= page_size,
+            total_equipements= total,
+            total_pages= (total + page_size - 1) // page_size,
+            equipos= equipos
+        )
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
