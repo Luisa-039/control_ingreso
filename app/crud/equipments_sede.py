95a845bf878ca80c9b86c385813fb41be725b3cd
Jeferson Rios <jefry1795@hotmail.com>
2026-03-10 10:55:07 -0500

    Refactor equipment functions and add pagination

--- app/crud/equipments_sede.py
+++ app/crud/equipments_sede.py
@@ -4,12 +4,12 @@ from typing import Optional
 from sqlalchemy.exc import SQLAlchemyError
 import logging
 
-from app.schemas.equipments_sede import Equipo_sedeCreate, Equipo_sedeUpdate, TipoEquipo_sede
+from app.schemas.equipments_sede import Equipo_sedeCreate, Equipo_sedeUpdate, TipoEquipo_sede, Estado_equip_sede
 
 logger = logging.getLogger(__name__)
 
 def create_equipment_sede(db: Session, 
-                     user: Equipo_sedeCreate,
+                     equipo_sede: Equipo_sedeCreate,
                      ) -> Optional[bool]:
     try:
         query = text("""
@@ -22,7 +22,7 @@ def create_equipment_sede(db: Session,
                 :descripcion, :marca_modelo, :fecha_registro, :estado
             )
         """)
-        db.execute(query, user.model_dump())
+        db.execute(query, equipo_sede.model_dump())
         db.commit()
         return True
     except SQLAlchemyError as e:
@@ -36,6 +36,7 @@ def get_equipment_sede_by_cod_barras(db: Session, codigo_barras: str):
                      FROM equipos_sede_inv 
                      WHERE codigo_barras_equipo = :codigo_barras""")
         result = db.execute(query, {"codigo_barras": codigo_barras}).mappings().first()
+        print([e.estado for e in result])
         return result
     except SQLAlchemyError as e:
         logger.error(f"Error al obtener equipo por código de barras: {e}")
@@ -43,8 +44,9 @@ def get_equipment_sede_by_cod_barras(db: Session, codigo_barras: str):
 
 def get_equipment_sede_by_serial(db: Session, serial_eq: str):
     try:
-        query = text("""SELECT *
-                     FROM equipos_sede_inv 
+        query = text("""SELECT *, s.nombre
+                    FROM equipos_sede_inv as eq
+                    INNER JOIN sedes as s ON eq.sede_id = s.id_sede
                      WHERE serial = :equipo_serial""")
         result = db.execute(query, {"equipo_serial": serial_eq}).mappings().first()
         return result
@@ -54,10 +56,12 @@ def get_equipment_sede_by_serial(db: Session, serial_eq: str):
 
 def get_all_equipments_sede(db: Session):
     try:
-        query = text("""SELECT *
-                     FROM equipos_sede_inv 
+        query = text("""SELECT eq.*, s.nombre
+                    FROM equipos_sede_inv as eq
+                    INNER JOIN sedes as s ON eq.sede_id = s.id_sede 
                      """)
         result = db.execute(query).mappings().all()
+        print([e.estado for e in result])
         return result
     except SQLAlchemyError as e:
         logger.error(f"Error al obtener los datos de los equipos: {e}")
@@ -87,14 +91,14 @@ def update_equip_sede_by_cod_barras(db: Session, cod_barras: str, equipment: Equ
         logger.error(f"Error al actualizar el equipo {cod_barras}: {e}")
         raise Exception("Error de base de datos al actualizar el equipo")
     
-def update_estado_equip_sede(db:Session, id_equip: str, estado_equipo: bool):
+def update_estado_equip_sede(db:Session, id_equip: int, estado_equipo: Estado_equip_sede):
     try:
         sentencia = text("""
             UPDATE equipos_sede_inv
             SET estado = :estado_eq
             WHERE id_equipo_sede = :equipo_id
         """)
-        result = db.execute(sentencia, {"estado_eq": estado_equipo, "equipo_id": id_equip})
+        result = db.execute(sentencia, {"estado_eq": estado_equipo.value, "equipo_id": id_equip})
         db.commit()
 
         return result.rowcount > 0
@@ -103,30 +107,6 @@ def update_estado_equip_sede(db:Session, id_equip: str, estado_equipo: bool):
         db.rollback()
         logger.error(f"Error al cambiar el estado del equipo {id_equip}: {e}")
         raise Exception("Error de base de datos al cambiar el estado del equipo")
-    
-# def update_equip_by_serial(db: Session, serial_equip: str, equipment: Equipo_sedeUpdate) -> Optional[bool]:
-#     try:
-#         equipment_data = equipment.model_dump(exclude_unset=True)
-#         if not equipment_data:
-#             return False 
-
-#         set_clauses = ", ".join([f"{key} = :{key}" for key in equipment_data.keys()])
-#         sentencia = text(f"""
-#             UPDATE equipos_externos 
-#             SET {set_clauses}
-#             WHERE serial = :serial_eq
-#         """)
-
-#         equipment_data["serial_eq"] = serial_equip
-
-#         result = db.execute(sentencia, equipment_data)
-#         db.commit()
-
-#         return result.rowcount > 0
-#     except SQLAlchemyError as e:
-#         db.rollback()
-#         logger.error(f"Error al actualizar el estado del equipo {serial_equip}: {e}")
-#         raise Exception("Error de base de datos al actualizar el estado del equipo")
     
 def update_equip_sede_by_id(db: Session, equipo_id: int, equipment: Equipo_sedeUpdate) -> Optional[bool]:
     try:
@@ -153,8 +133,9 @@ def update_equip_sede_by_id(db: Session, equipo_id: int, equipment: Equipo_sedeU
      
 def get_equipment_sede_by_tipo(db: Session, tipo_equip: TipoEquipo_sede):
     try:
-        query = text("""SELECT *
-                     FROM equipos_sede_inv 
+        query = text("""SELECT *s.nombre
+                     FROM equipos_sede_inv as eq 
+                     INNER JOIN sedes as s ON eq.sede_id = s.id_sede
                      WHERE categoria = :equipo_tipo""")
         result = db.execute(query, {"equipo_tipo": tipo_equip}).mappings().first()
         return result
@@ -162,3 +143,35 @@ def get_equipment_sede_by_tipo(db: Session, tipo_equip: TipoEquipo_sede):
         logger.error(f"Error al obtener equipo por id: {e}")
         raise Exception("Error de base de datos al obtener el equipo por id")
 
+def get_all_equipements_sede_pag(db: Session, skip:int = 0, limit = 10):
+    """
+    Obtiene los equipos con paginación.
+    También realizar una segunda consulta para contar total de equipos.
+    compatible con PostgreSQL, MySQL y SQLite 
+    """
+    try: 
+        
+        count_query = text("""SELECT COUNT(id_equipo_sede) AS total 
+                     FROM equipos_sede_inv
+                     """)
+        total_result = db.execute(count_query).scalar()
+
+        #2 Consultar equipos
+        data_query = text("""SELECT eq.id_equipo_sede, eq.serial, eq.codigo_barras_equipo, eq.descripcion,
+                          eq.categoria, eq.marca_modelo, eq.sede_id, eq.fecha_registro,
+                          eq.estado, s.nombre
+                          FROM equipos_sede_inv as eq
+                          INNER JOIN sedes as s ON eq.sede_id = s.id_sede
+                          LIMIT :limit OFFSET :skip
+                        """)
+        equipos_list = db.execute(data_query,{"skip": skip, "limit": limit}).mappings().all()
+        
+        return {
+                "total": total_result or 0,
+                "equipos": equipos_list
+            }
+    except SQLAlchemyError as e:
+        logger.error(f"Error al obtener los equipos: {e}", exc_info=True)
+        raise Exception("Error de base de datos al obtener los equipos")
+
+ 
