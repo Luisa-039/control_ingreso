ace795b56b6b6a81f7d09bd0bb0098655150ee30
Luisa Sánchez <168785270+Luisa-039@users.noreply.github.com>
2026-03-18 11:36:38 -0500

    Update FastAPI routers and CORS settings

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.router import users, auth, person, sede, center, equipments, autorizacion_salida, movements, roles, access
+from app.router import users, auth, person, sede, center, equipments, equipments_sede, access, roles, autorizacion_salida, movements, permisos, department, type, modulo, categorie
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5173"],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
@@ -16,13 +18,19 @@ app.include_router(users.router, prefix="/users", tags=["users"])
 app.include_router(roles.router, prefix="/rol", tags=["roles"])
 app.include_router(person.router, prefix="/person", tags=["person"])
 app.include_router(sede.router, prefix="/sede", tags=["sede"])
-app.include_router(center.router, prefix="/center", tags=["center"])
 app.include_router(equipments.router, prefix="/equipments", tags=["equipments"])
+app.include_router(center.router, prefix="/center", tags=["center"])
+app.include_router(equipments_sede.router, prefix="/equipments_sede", tags=["equipments_sede"])
 app.include_router(autorizacion_salida.router, prefix="/autorizacion_salida", tags=["autorizacion_salida"])
 app.include_router(movements.router, prefix="/movements", tags=["movements"])
 app.include_router(access.router, prefix="/access", tags=["access"])
+app.include_router(permisos.router, prefix="/permisos", tags=["permisos"])
+app.include_router(department.router, prefix="/deparment", tags=["departamentos"])
+app.include_router(type.router, prefix="/type", tags=["tipo_movimientos"])
+app.include_router(modulo.router, prefix="/modulo", tags=["modulos"])
+app.include_router(categorie.router, prefix="/categoria", tags=["categorias"])
 
-app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads"
+app.mount("/uploads", StaticFiles(directory="uploads"), name="uploads")
 
 @app.get("/")
 def read_root():
