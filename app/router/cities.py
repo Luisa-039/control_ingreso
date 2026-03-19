a7fc31be716e5f2804d61606090e35de9f31a842
Jeferson Rios <jefry1795@hotmail.com>
2026-03-19 10:59:38 -0500

    Implement pagination for cities retrieval
    Added pagination support for retrieving cities.

--- app/router/cities.py
+++ app/router/cities.py
@@ -1,10 +1,10 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.orm import Session
 from app.crud.permisos import verify_permissions
 from app.router.dependencies import get_current_user
 from app.core.database import get_db
-from app.schemas.cities import CitiesCreate, CitiesUpdate, CitiesOut
+from app.schemas.cities import CitiesCreate, CitiesUpdate, CitiesOut, PaginatedCities
 from app.schemas.users import UserOut
 from app.crud import cities as crud_cities
 from sqlalchemy.exc import SQLAlchemyError
@@ -102,3 +102,31 @@ def change_city_status(
         raise
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
+
+@router.get("/all_cities-pag", response_model=PaginatedCities)
+def get_cities_pag(
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+): 
+    try:        
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        skip = (page - 1) * page_size
+        data = crud_cities.get_all_cities_pag(db, skip=skip, limit=page_size)
+
+        total = data["total"]  
+        ciudades = data["ciudades"]
+
+        return PaginatedCities(
+            page= page,
+            page_size= page_size,
+            total_cities= total,
+            total_pages= (total + page_size - 1) // page_size,
+            ciudades= ciudades
+        )
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
