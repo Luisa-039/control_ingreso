ff52e9f4c3f9f3ac612ccbe60798c416632979e5
Jeferson Rios <jefry1795@hotmail.com>
2026-03-20 11:47:48 -0500

    Refactor inv_consumibles router with CRUD operations

--- app/router/inv_consumibles.py
+++ app/router/inv_consumibles.py
@@ -1,88 +1,128 @@
-from fastapi import APIRouter, Depends, HTTPException, status
-from sqlalchemy.orm import Session
-from sqlalchemy.exc import SQLAlchemyError
 from typing import List
-
-from app.schemas.permisos import PermisoCreate, PermisoOut, PermisoUpdate
-from app.schemas.users import UserOut
-from app.crud import modulo_permisos as modulo_permisos
-from app.crud.permisos import verify_permissions  # tu función actual de verificación
-from app.router.dependencies import get_current_user
+from fastapi import APIRouter, Depends, HTTPException, status, Query
+from sqlalchemy.orm import Session
 from app.core.database import get_db
+from app.crud.permisos import verify_permissions
+from app.router.dependencies import get_current_user
+from app.schemas.users import UserOut
+from app.schemas.inv_consumibles import Inv_consumibleCreate, Inv_consumibleUpdate, Inv_consumibleEstado, Inv_consumibleOut, PaginatedInv_consumibles
+from app.crud import inv_consumibles as crud_inv_consumibles
+from sqlalchemy.exc import SQLAlchemyError
 
 router = APIRouter()
-modulo = 2  # ID asignados
+modulo = 15
 
-# Crear permiso
 @router.post("/crear", status_code=status.HTTP_201_CREATED)
-def create_permiso(
-    permiso_data: PermisoCreate,
+def create_consumible(
+    consumible: Inv_consumibleCreate, 
     db: Session = Depends(get_db),
     user_token: UserOut = Depends(get_current_user)
 ):
-    id_rol = user_token.rol_id
-    if not verify_permissions(db, id_rol, modulo, 'insertar'):
-        raise HTTPException(status_code=401, detail="Usuario no autorizado para crear permisos")
     try:
-        modulo_permisos.create_permiso(db, permiso_data)
-        return {"message": "Permiso creado correctamente"}
-    except Exception as e:
-        if "permiso_existe" in str(e):
-            raise HTTPException(status_code=409, detail="Este permiso ya existe")
+        id_rol = user_token.rol_id       
+        if not verify_permissions(db, id_rol, modulo, 'insertar'):
+            raise HTTPException(status_code=401, detail= 'Usuario no autorizado')
+        
+        crud_inv_consumibles.create_inv_consumible(db, consumible)
+        return {"message": "Consumible registrado correctamente"}
+    except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+@router.get("/by-id",  response_model=Inv_consumibleOut)
+def scan_consumible(id_consumible: int, 
+                   db: Session = Depends(get_db),
+                   user_token: UserOut = Depends(get_current_user)):
+    try:
+        id_rol=user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        equipo = crud_inv_consumibles.get_inv_consumible_by_id(db, id_consumible)
+        if not equipo:
+            raise HTTPException(status_code=404, detail="Consumible no encontrado")
+        return equipo
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# Obtener todos los permisos
-@router.get("/all-permisos", response_model=List[PermisoOut])
-def get_all_permisos(
-    db: Session = Depends(get_db),
-    user_token: UserOut = Depends(get_current_user)
-):
-    id_rol = user_token.rol_id
-    if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
-        raise HTTPException(status_code=401, detail="Usuario no autorizado para ver permisos")
+@router.get("/all-inv_consumible",  response_model=List[Inv_consumibleOut])
+def scan_equipment(db: Session = Depends(get_db),
+                   user_token: UserOut = Depends(get_current_user)):
     try:
-        permisos = modulo_permisos.get_all_permisos(db)
-        return permisos
+        id_rol=user_token.rol_id
+
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        equipo = crud_inv_consumibles.get_all_inv_consumibles(db)
+        if not equipo:
+            raise HTTPException(status_code=404, detail="Consumibles no encontrados")
+        return equipo
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-# Obtener permiso por id_modulo e id_rol
-@router.get("/{id_modulo}/{id_rol}", response_model=PermisoOut)
-def get_permiso_by_ids(
-    id_modulo: int,
-    id_rol: int,
-    db: Session = Depends(get_db),
-    user_token: UserOut = Depends(get_current_user)
-):
-    id_rol_user = user_token.rol_id
-    if not verify_permissions(db, id_rol_user, modulo, 'seleccionar'):
-        raise HTTPException(status_code=401, detail="Usuario no autorizado para ver permisos")
+@router.put("/by_id/{id_consumible}")
+def update_consumible_by_id(id_consumible: int, 
+                 consumible: Inv_consumibleUpdate, 
+                 db: Session = Depends(get_db),
+                 user_token: UserOut = Depends(get_current_user)):
     try:
-        permiso = modulo_permisos.get_permiso_by_ids(db, id_modulo, id_rol)
-        if not permiso:
-            raise HTTPException(status_code=404, detail="Permiso no encontrado")
-        return permiso
-    except SQLAlchemyError as e:
-        raise HTTPException(status_code=500, detail=str(e))
-
-# Actualizar permiso
-@router.put("/{id_modulo}/{id_rol}")
-def update_permiso(
-    id_modulo: int,
-    id_rol: int,
-    permiso_data: PermisoUpdate,
-    db: Session = Depends(get_db),
-    user_token: UserOut = Depends(get_current_user)
-):
-    id_rol_user = user_token.rol_id
-    if not verify_permissions(db, id_rol_user, modulo, 'actualizar'):
-        raise HTTPException(status_code=401, detail="Usuario no autorizado para actualizar permisos")
-    try:
-        success = modulo_permisos.update_permiso(db, id_modulo, id_rol, permiso_data)
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'actualizar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        success = crud_inv_consumibles.update_inv_consumible_by_id(db, id_consumible, consumible)
         if not success:
-            raise HTTPException(status_code=404, detail="Permiso no encontrado")
-        return {"message": "Permiso actualizado correctamente"}
+            raise HTTPException(status_code=400, detail="No se pudo actualizar el consumible")
+        return {"message": "Consumible actualizado correctamente"}
     except SQLAlchemyError as e:
         raise HTTPException(status_code=500, detail=str(e))
+
+@router.put("/estado/{id_consumible}", status_code=status.HTTP_200_OK)
+def estado_consumible(
+    id_consumible: int,
+    estado: Inv_consumibleEstado,
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+):
+    try:
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'actualizar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        success = crud_inv_consumibles.update_estado_consumible(db, id_consumible, estado.estado)
+        if not success:
+            raise HTTPException(status_code=404, detail="Consumible no encontrado")
+
+        return {"message": f"Estado del consumible actualizado a {estado.estado}"}
+    except SQLAlchemyError as e:
+            raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/all_consumibles-pag", response_model=PaginatedInv_consumibles)
+def get_consumibles_pag(
+    page: int = Query(1, ge=1),
+    page_size: int = Query(10, ge=1, le=100),
+    db: Session = Depends(get_db),
+    user_token: UserOut = Depends(get_current_user)
+): 
+    try:        
+        id_rol = user_token.rol_id
+        if not verify_permissions(db, id_rol, modulo, 'seleccionar'):
+            raise HTTPException(status_code=401, detail="Usuario no autorizado")
+        
+        skip = (page - 1) * page_size
+        data = crud_inv_consumibles.get_all_consumible_pag(db, skip=skip, limit=page_size)
+
+        total = data["total"]  
+        consumibles = data["consumibles"]
+
+        return PaginatedInv_consumibles(
+            page= page,
+            page_size= page_size,
+            total_consumibles= total,
+            total_pages= (total + page_size - 1) // page_size,
+            consumibles= consumibles
+        )
+    except SQLAlchemyError as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
